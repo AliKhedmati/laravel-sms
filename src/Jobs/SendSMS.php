c34cff721c8ca29b80dd9b692a97ed3f11168e1e
Ali Khedmati <ali@khedmati.ir>
2022-07-05 04:11:56 +0430

    Job has been added.

---
+++ src/Jobs/SendSMS.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Alikhedmati\SMS\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendSMS implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function handle()
+    {
+        
+    }
+}
---
+++ src/Providers/SMSIR.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Alikhedmati\SMS\Providers;
+
+use Alikhedmati\SMS\Contracts\SMSProviderInterface;
+
+class SMSIR implements SMSProviderInterface
+{
+
+}
